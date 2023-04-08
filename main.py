7219998443fe691fffa7a19f696e5dd3f028d817
Mohammad Reza Taesiri <mtaesiri@gmail.com>
2023-04-08 04:47:54 +0000

    update

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import os
 import requests
+import re
+from io import BytesIO
 import xml.etree.ElementTree as ET
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
+import matplotlib.pyplot as plt
+from shutil import which
+import tempfile
+import subprocess
 
 
 def extract_arxiv_abstract(url):
@@ -51,13 +60,51 @@ def arxiv_abstract_to_speech(arxiv_url, api_key):
   return response
 
 
+def abstract_to_pdf(abstract, output_filename='abstract.pdf'):
+  if not which("pdflatex"):
+    raise Exception("pdflatex not found. Please install TeX Live or MiKTeX.")
+
+  latex_template = r"""
+        \documentclass{{standalone}}
+        \usepackage[utf8]{{inputenc}}
+        \usepackage{{amsmath}}
+        \usepackage{{amssymb}}
+        \usepackage{{hyperref}}
+        \usepackage{{varwidth}}
+        \begin{{document}}
+        \begin{{varwidth}}{{\linewidth}}
+        {content}
+        \end{{varwidth}}
+        \end{{document}}
+        """
+
+  latex_content = latex_template.format(content=abstract)
+
+  with tempfile.TemporaryDirectory() as temp_dir:
+    tex_file = os.path.join(temp_dir, "abstract.tex")
+    with open(tex_file, "w") as f:
+      f.write(latex_content)
+
+    cmd = [
+      "pdflatex", "-interaction=batchmode", "-output-directory", temp_dir,
+      tex_file
+    ]
+    subprocess.run(cmd, check=True)
+
+    pdf_file = os.path.join(temp_dir, "abstract.pdf")
+    os.rename(pdf_file, output_filename)
+
+
 # Example usage:
 arxiv_url = 'https://arxiv.org/abs/2304.03279'
 api_key = 'xXx'
 
-response = arxiv_abstract_to_speech(arxiv_url, api_key)
-print(response.status_code)
+# response = arxiv_abstract_to_speech(arxiv_url, api_key)
+# print(response.status_code)
 
-# Save the response content (audio file) to disk
-with open('abstract.mp3', 'wb') as f:
-  f.write(response.content)
+# # Save the response content (audio file) to disk
+# with open('abstract.mp3', 'wb') as f:
+#   f.write(response.content)
+
+abstract = extract_arxiv_abstract(arxiv_url)
+abstract_to_pdf(abstract, 'abstractx.pdf')
